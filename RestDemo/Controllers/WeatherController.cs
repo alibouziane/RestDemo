8e7c8dd65883ab5491c7e31751f8685ecea8257a
Bouziane <Bouziane@DESK-BOUZIANE>
2020-09-23 17:20:36 +0200

    Ajoutez des fichiers projet.

---
+++ RestDemo/Controllers/WeatherController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RestDemo.Models;
+
+namespace RestDemo.Controllers
+{
+    public class WeatherController : ApiController
+    {
+        // GET: api/Weather
+        public IEnumerable<WeatherInfo> Get()
+        {
+            var weatherInfos = new List<WeatherInfo>();
+            for (var i = 0; i < 10; i++)
+            {
+                weatherInfos.Add(new WeatherInfo()
+                {
+                    Location = $"Location {i}",
+                    Degree = i * 23 / 17,
+                    DateTime = DateTime.UtcNow
+                });
+            }
+            return weatherInfos;
+        }
+
+        // GET: api/Weather/5
+        public WeatherInfo Get(int id)
+        {
+            return new WeatherInfo()
+            {
+                Location = $"Location {id}",
+                Degree = id * 23 / 17,
+                DateTime = DateTime.UtcNow
+            };
+        }
+
+        //    // POST: api/Weather
+        //    public void Post([FromBody]string value)
+        //    {
+        //    }
+
+        //    // PUT: api/Weather/5
+        //    public void Put(int id, [FromBody]string value)
+        //    {
+        //    }
+
+        //    // DELETE: api/Weather/5
+        //    public void Delete(int id)
+        //    {
+        //    }
+    }
+}
